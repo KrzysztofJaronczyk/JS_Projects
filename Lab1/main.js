4e229afcdd11891cbfee4985a53b0d3332531164
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-10-01 16:10:37 +0200

    Finish the complete task

--- Lab1/main.js
+++ Lab1/main.js
@@ -1,32 +1,82 @@
 const container = document.getElementById('container')
-const liczba1 = document.getElementById('1')
-const liczba2 = document.getElementById('2')
-const liczba3 = document.getElementById('3')
-const liczba4 = document.getElementById('4')
+const result = document.getElementById('result')
 
-const button = document.querySelector('button')
+const addFieldButton = document.querySelector('button')
+const resetButton = document.getElementById('reset')
 
-button.addEventListener('click', () => {
-	const suma = parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)
-	const srednia = suma / 4
-	const min = Math.min(liczba1.value, liczba2.value, liczba3.value, liczba4.value)
-	const max = Math.max(liczba1.value, liczba2.value, liczba3.value, liczba4.value)
+resetButton.addEventListener('click', () => {
+	location.reload()
+})
 
-	container.innerHTML = ''
-	const wynik = document.createElement('p')
-	wynik.innerHTML = `Suma: ${suma} Średnia: ${srednia} Min: ${min} Max: ${max}`
-	container.appendChild(wynik)
+addFieldButton.addEventListener('click', () => {
+	const containerDiv = document.createElement('div')
+	const newField = document.createElement('input')
+	const deleteButton = document.createElement('button')
+	const number = container.children.length + 1
+
+	deleteButton.innerHTML = 'Usuń'
+	deleteButton.id = 'Delete' + number
+	newField.type = 'number'
+	newField.id = number
+	newField.placeholder = 'Wpisz liczbę'
+	//add new fields in 1 line
+	containerDiv.appendChild(newField)
+	containerDiv.appendChild(deleteButton)
+
+	container.appendChild(containerDiv)
+
+	deleteButton.addEventListener('click', () => {
+		newField.remove()
+		deleteButton.remove()
+		//count numbers
+		const numbers = []
+		document.querySelectorAll('input').forEach(input => {
+			if (input.value) {
+				numbers.push(parseInt(input.value))
+				input.style.backgroundColor = 'white'
+
+				const suma = numbers.reduce((a, b) => a + b, 0)
+				const srednia = suma / numbers.length
+				const min = Math.min(...numbers)
+				const max = Math.max(...numbers)
+				result.innerHTML = ''
+				const wynik = document.createElement('p')
+				wynik.innerHTML = `Suma: ${suma} Średnia: ${srednia} Min: ${min} Max: ${max}`
+				result.appendChild(wynik)
+			} else {
+				input.style.backgroundColor = 'red'
+			}
+		})
+	})
 })
 
 //if liczba value has changed
 document.addEventListener('change', () => {
-	const suma = parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)
-	const srednia = suma / 4
-	const min = Math.min(liczba1.value, liczba2.value, liczba3.value, liczba4.value)
-	const max = Math.max(liczba1.value, liczba2.value, liczba3.value, liczba4.value)
-	container.innerHTML = ''
+	const numbers = []
+	document.querySelectorAll('input').forEach(input => {
+		if (input.value) {
+			numbers.push(parseInt(input.value))
+			input.style.backgroundColor = 'white'
 
-	const wynik = document.createElement('p')
-	wynik.innerHTML = `Suma: ${suma} Średnia: ${srednia} Min: ${min} Max: ${max}`
-	container.appendChild(wynik)
+			const suma = numbers.reduce((a, b) => a + b, 0)
+			const srednia = suma / numbers.length
+			const min = Math.min(...numbers)
+			const max = Math.max(...numbers)
+			result.innerHTML = ''
+			const wynik = document.createElement('p')
+			wynik.innerHTML = `Suma: ${suma} Średnia: ${srednia} Min: ${min} Max: ${max}`
+			result.appendChild(wynik)
+		} else {
+			input.style.backgroundColor = 'red'
+		}
+	})
+	// const suma = parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value)
+	// const srednia = suma / 3
+	// const min = Math.min(liczba1.value, liczba2.value, liczba3.value)
+	// const max = Math.max(liczba1.value, liczba2.value, liczba3.value)
+	// result.innerHTML = ''
+
+	// const wynik = document.createElement('p')
+	// result.innerHTML = `Suma: ${suma} Średnia: ${srednia} Min: ${min} Max: ${max}`
+	// container.appendChild(wynik)
 })
