e9e7c8e079062a4b3414ea399aee26ccdc9c9878
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-12-08 18:02:52 +0100

    Add another functionalities

--- Lab4-Notes/script.js
+++ Lab4-Notes/script.js
@@ -1,10 +1,11 @@
 const addBtn = document.querySelector('.add')
 const saveBtn = document.querySelector('.save')
 const cancelBtn = document.querySelector('.cancel')
-const deleteBtns = document.getElementsByClassName('.delete-note')
 const deleteAllBtn = document.querySelector('.delete-all')
 
 const noteArea = document.querySelector('.note-area')
+const pinnedArea = document.querySelector('.pinned-area')
+
 const notePanel = document.querySelector('.note-panel')
 const category = document.querySelector('#category')
 const textarea = document.querySelector('#text')
@@ -12,6 +13,7 @@ const error = document.querySelector('.error')
 
 let selectedValue
 let cardId = 0
+let editMode = false
 
 const openPanel = () => {
 	notePanel.style.display = 'flex'
@@ -25,39 +27,150 @@ const closePanel = () => {
 }
 
 const addNote = () => {
-	if (textarea.value !== '' && category.value !== '0') {
-		createNote()
+	if (textarea.value !== '' && category.selectedIndex !== 0) {
+		if (editMode) {
+			const noteToEdit = document.getElementById(editMode)
+			noteToEdit.querySelector('.note-title').innerText = selectedValue
+			noteToEdit.querySelector('.note-body').innerText = textarea.value
+			checkColor(noteToEdit)
+			editMode = false
+		} else {
+			createNote()
+		}
 		closePanel()
 	} else {
 		error.style.visibility = 'visible'
 	}
 }
-
 const createNote = () => {
 	const newNote = document.createElement('div')
 	newNote.classList.add('note')
 	newNote.setAttribute('id', cardId)
-	noteArea.appendChild(newNote)
 	cardId++
 	newNote.innerHTML = ` <div class="note-header">
-	<div class="note-title">${selectedValue}</div>
-	<button class="delete-note" onclick="deleteNote(${newNote.id})">
-		<i class="fas fa-times icon"></i>
-	</button>
-</div>
-<div class="note-body">
-	${textarea.value}
-</div>`
+        <div class="note-title">${selectedValue}</div>
+        <button class="delete-note" onclick="deleteNote(${newNote.id})">
+            <i class="fas fa-times icon"></i>
+        </button>
+        <button class="edit-note" onclick="loadNote(${newNote.id})">
+            <i class="fas fa-edit icon"></i>
+        </button>
+        <button class="pin-note" onclick="pinNote(${newNote.id})">
+            <i class="fas fa-thumbtack icon"></i>
+        </button>
+    </div>
+    <div class="note-body">
+        ${textarea.value}
+    </div>`
 	checkColor(newNote)
+
+	noteArea.appendChild(newNote)
+
+	saveToLocalStorage()
+}
+
+const loadNote = id => {
+	const noteToEdit = document.getElementById(id)
+	openPanel()
+	textarea.value = noteToEdit.querySelector('.note-body').innerText
+	const noteTitle = noteToEdit.querySelector('.note-title').innerText
+
+	for (let i = 0; i < category.options.length; i++) {
+		if (category.options[i].text === noteTitle) {
+			category.selectedIndex = i
+			break
+		}
+	}
+	editMode = id
+}
+
+const pinNote = id => {
+	const noteToPin = document.getElementById(id)
+	const isPinned = noteToPin.classList.toggle('pinned')
+
+	if (isPinned) {
+		pinnedArea.appendChild(noteToPin)
+	} else {
+		noteArea.appendChild(noteToPin)
+	}
+
+	saveToLocalStorage()
+}
+const saveToLocalStorage = () => {
+	const pinnedNotes = []
+	const regularNotes = []
+
+	document.querySelectorAll('.note').forEach(note => {
+		const noteObj = {
+			id: note.id,
+			title: note.querySelector('.note-title').innerText,
+			content: note.querySelector('.note-body').innerText,
+			color: note.style.backgroundColor,
+			pin: note.classList.contains('pinned'),
+			createdAt: new Date().toISOString(),
+		}
+
+		if (noteObj.pin) {
+			pinnedNotes.push(noteObj)
+		} else {
+			regularNotes.push(noteObj)
+		}
+	})
+	localStorage.setItem('pinnedNotes', JSON.stringify(pinnedNotes))
+	localStorage.setItem('regularNotes', JSON.stringify(regularNotes))
+}
+const loadFromLocalStorage = () => {
+	const storedPinnedNotes = localStorage.getItem('pinnedNotes')
+	const storedRegularNotes = localStorage.getItem('regularNotes')
+
+	loadNotesFromStorage(storedPinnedNotes, pinnedArea)
+	loadNotesFromStorage(storedRegularNotes, noteArea)
+}
+
+const loadNotesFromStorage = (storedNotes, targetArea) => {
+	if (storedNotes) {
+		const notes = JSON.parse(storedNotes)
+		notes.forEach(note => {
+			const newNote = createNoteElement(note)
+			targetArea.appendChild(newNote)
+		})
+	}
+}
+
+const createNoteElement = noteObj => {
+	const newNote = document.createElement('div')
+	newNote.classList.add('note')
+	newNote.setAttribute('id', noteObj.id)
+	newNote.style.backgroundColor = noteObj.color
+	newNote.innerHTML = `
+        <div class="note-header">
+            <div class="note-title">${noteObj.title}</div>
+            <button class="delete-note" onclick="deleteNote(${noteObj.id})">
+                <i class="fas fa-times icon"></i>
+            </button>
+            <button class="edit-note" onclick="editNote(${noteObj.id})">
+                <i class="fas fa-edit icon"></i>
+            </button>
+            <button class="pin-note" onclick="pinNote(${noteObj.id})">
+                <i class="fas fa-thumbtack icon"></i>
+            </button>
+        </div>
+        <div class="note-body">
+            ${noteObj.content}
+        </div>`
+
+	if (noteObj.pin) {
+		newNote.classList.add('pinned')
+	}
+
+	return newNote
 }
 
 const selectValue = () => {
 	selectedValue = category.options[category.selectedIndex].text
-	// console.log(selectedValue)
 }
 
 const checkColor = note => {
-	// console.log(selectedValue)
 	switch (selectedValue) {
 		case 'Work':
 			note.style.backgroundColor = 'green'
@@ -65,19 +178,33 @@ const checkColor = note => {
 		case 'Free Time':
 			note.style.backgroundColor = 'red'
 			break
+		case 'Study':
+			note.style.backgroundColor = 'rgb(255, 243, 0)'
+			break
+		case 'Other':
+			note.style.backgroundColor = 'orange'
+			break
+		default:
+			note.style.backgroundColor = 'white'
+			break
 	}
 }
 
 const deleteNote = id => {
 	const noteToDelete = document.getElementById(id)
 	noteArea.removeChild(noteToDelete)
+	saveToLocalStorage()
 }
 
 const deleteAllNotes = () => {
 	noteArea.innerHTML = ''
+	pinnedArea.innerHTML = ''
+	saveToLocalStorage()
+	location.reload()
 }
 
 addBtn.addEventListener('click', openPanel)
 cancelBtn.addEventListener('click', closePanel)
 saveBtn.addEventListener('click', addNote)
 deleteAllBtn.addEventListener('click', deleteAllNotes)
+window.addEventListener('load', loadFromLocalStorage)
