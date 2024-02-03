8526c6f73d604e10b38c2e525314ae75564d717e
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-02-03 20:21:49 +0100

    Small update

--- Lab7-WeatherApp/script.js
+++ Lab7-WeatherApp/script.js
@@ -6,6 +6,7 @@ const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 const wrapper = document.querySelector('.wrapper')
+const deleteBtn = document.querySelector('.fa-circle-xmark')
 
 const addBtn = document.querySelector('.add')
 const refreshBtn = document.querySelector('.refresh')
@@ -15,39 +16,39 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=14296e27415e5e7f9c5595a6105bb271'
 const API_UNITS = '&units=metric'
 
-const getWeather = (e) => {
-	const inputElement = e.target;
-	const city = inputElement.value;
-	const URL = API_LINK + city + API_KEY + API_UNITS;
+const getWeather = e => {
+	const inputElement = e.target
+	const city = inputElement.value
+	const URL = API_LINK + city + API_KEY + API_UNITS
 
-	axios.get(URL)
+	axios
+		.get(URL)
 		.then(res => {
-			const container = inputElement.closest('.container');
-			const cityName = container.querySelector('.city-name');
-			const weather = container.querySelector('.weather');
-			const temperature = container.querySelector('.temperature');
-			const humidity = container.querySelector('.humidity');
-			const photo = container.querySelector('.photo');
-			const warning = container.querySelector('.warning');
+			const container = inputElement.closest('.container')
+			const cityName = container.querySelector('.city-name')
+			const weather = container.querySelector('.weather')
+			const temperature = container.querySelector('.temperature')
+			const humidity = container.querySelector('.humidity')
+			const photo = container.querySelector('.photo')
+			const warning = container.querySelector('.warning')
 
-			const temp = res.data.main.temp;
-			const hum = res.data.main.humidity;
-			const status = Object.assign({}, ...res.data.weather);
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
 
-			cityName.textContent = res.data.name;
-			temperature.textContent = Math.floor(temp) + '°C';
-			humidity.textContent = hum + '%';
-			weather.textContent = status.main;
-			photo.src = checkWeatherIcon(status.main);
-			warning.textContent = '';
-			e.target.value = '';
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(temp) + '°C'
+			humidity.textContent = hum + '%'
+			weather.textContent = status.main
+			photo.src = checkWeatherIcon(status.main)
+			warning.textContent = ''
+			e.target.value = ''
 		})
 		.catch(() => {
-			const warning = inputElement.nextElementSibling; // Uprzednio zdefiniuj, gdzie dokładnie znajduje się element warning
-			warning.textContent = 'Enter the correct city name';
-		});
-};
-
+			const warning = inputElement.nextElementSibling // Uprzednio zdefiniuj, gdzie dokładnie znajduje się element warning
+			warning.textContent = 'Enter the correct city name'
+		})
+}
 
 function checkWeatherIcon(weather) {
 	switch (weather) {
@@ -76,10 +77,11 @@ function addNewCity() {
 		wrapper.innerHTML += `
 	<div class="container id=${totalCities}">
 	<div class="top">
+	<i class="fa-regular fa-circle-xmark"></i>
 		<div class="main-info">
 			<div>
 				<h3 class="city-name"></h3>
-				<input type="text" id=${totalCities} placeholder="Enter city name...">
+				<input type="text" placeholder="Enter city name...">
 				<p class="warning"></p>
 			</div>
 			<img src="./img/unknown.png" alt="Picture of actual weather" class="photo">
@@ -108,13 +110,22 @@ refreshBtn.addEventListener('click', () => {
 	location.reload()
 })
 
+wrapper.addEventListener('click', e => {
+	if (e.target.classList.contains('fa-circle-xmark')) {
+		const container = e.target.closest('.container')
+		container.remove()
+	}
+})
+
 deleteAllBtn.addEventListener('click', () => {
 	wrapper.innerHTML = ''
 })
 
 addBtn.addEventListener('click', addNewCity)
+
 wrapper.addEventListener('keyup', e => {
-    if (e.key === 'Enter') {
-        getWeather(e);
-    }
-});
+	if (e.key === 'Enter') {
+		getWeather(e)
+	}
+})
+
